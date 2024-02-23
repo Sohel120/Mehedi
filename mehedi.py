25bb309cb9e21ffc1b581de149836e1404dc60d0
Sohel120 <139029146+Sohel120@users.noreply.github.com>
2024-02-23 23:42:55 +0600

    Add files via upload

---
+++ mehedi.py
@@ -0,0 +1,13 @@
+logo = f"""
+\033[1;33m                                                                                                              
+                      ,,                ,,                                         ,,                       ,,    ,,  
+\033[1;33m.M"""bgd         `7MM               *MM                `7MMM.     ,MMF'       `7MM                     `7MM    db  
+\033[1;33m,MI    "Y           MM                MM        M.     ,M MMMb    dPMM           MM                       MM        
+\033[1;33m`MMb.      ,pW"Wq.  MMpMMMb.  .gP"Ya  MM,dMMb.   `M. ,M'  M YM   ,M MM  .gP"Ya   MMpMMMb.  .gP"Ya    ,M""bMM  `7MM  
+ \033[1;33m `YMMNq. 6W'   `Wb MM    MM ,M'   Yb MM    `Mb    >M<    M  Mb  M' MM ,M'   Yb  MM    MM ,M'   Yb ,AP    MM    MM  
+\033[1;33m.     `MM 8M     M8 MM    MM 8M"""""" MM     M8  ,M' `M.  M  YM.P'  MM 8M""""""  MM    MM 8M"""""" 8MI    MM    MM  
+\033[1;33mMb     dM YA.   ,A9 MM    MM YM.    , MM.   ,M9 M'     `M M  `YM'   MM YM.    ,  MM    MM YM.    , `Mb    MM    MM  
+\033[1;33mP"Ybmmd"   `Ybmd9'.JMML  JMML.`Mbmmd' P^YbmdP'          .JML. `'  .JMML.`Mbmmd'.JMML  JMML.`Mbmmd'  `Wbmd"MML..JMML.
+\033[1;35m──────────────────────────────────────────────────"""
+                                                                                                                    
+                                                                                                                    
